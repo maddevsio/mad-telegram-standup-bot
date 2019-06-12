fca045d5e177082e081caa0ed76a978153e0159b
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-06-12 19:36:40 +0600

    Add migration to change text from varchar to text

---
+++ migrations/00004_standups.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+ALTER TABLE `standups` MODIFY `text` TEXT COLLATE utf8mb4_unicode_ci NOT NULL;
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+ALTER TABLE `standups` MODIFY `text` VARCHAR(3000) COLLATE utf8mb4_unicode_ci NOT NULL;
