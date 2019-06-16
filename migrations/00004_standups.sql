2f75416a3a05cc6ac96d50a053eec19da0ae97da
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-06-16 14:05:47 +0600

    Add charset to db connect string to fix #1

--- migrations/00004_standups.sql
+++ migrations/00004_standups.sql
@@ -4,4 +4,4 @@ ALTER TABLE `standups` MODIFY `text` TEXT COLLATE utf8mb4_unicode_ci NOT NULL;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE `standups` MODIFY `text` VARCHAR(3000) COLLATE utf8mb4_unicode_ci NOT NULL;
+ALTER TABLE `standups` MODIFY `text` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
