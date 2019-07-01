9a87f2d777ddff14a1e072d2ad0abe1bee697977
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-07-01 23:04:27 +0600

    Add warning functionality to fix #8

--- bot/notifications.go
+++ bot/notifications.go
@@ -13,6 +13,7 @@ import (
 )
 
 const allowedSkips = 3
+const warnPeriod = 10 // 10 minutes before the deadline
 
 //StartWatchers looks for new gropus from the channel and start watching it
 func (b *Bot) StartWatchers() {
@@ -34,11 +35,80 @@ func (b *Bot) trackStandupersIn(team *model.Team) {
 	for {
 		select {
 		case <-ticker:
+			b.WarnGroup(team.Group, time.Now())
 			b.NotifyGroup(team.Group, time.Now())
 		case <-team.QuitChan:
 			log.Info("Finish working with the group: ", team.QuitChan)
 			return
 		}
+	}
+}
+
+//WarnGroup launches go routines that warns standupers
+//about upcoming deadlines
+func (b *Bot) WarnGroup(group *model.Group, t time.Time) {
+	if int(t.Weekday()) == 6 || int(t.Weekday()) == 0 {
+		return
+	}
+
+	if group.StandupDeadline == "" {
+		return
+	}
+	w := when.New(nil)
+	w.Add(en.All...)
+	w.Add(ru.All...)
+
+	r, err := w.Parse(group.StandupDeadline, time.Now())
+	if err != nil {
+		log.Errorf("Unable to parse channel standup time [%v]: [%v]", group.StandupDeadline, err)
+		return
+	}
+
+	if r == nil {
+		log.Errorf("Could not find matches. Channel standup time: [%v]", group.StandupDeadline)
+		return
+	}
+
+	t = t.Add(warnPeriod * time.Minute)
+
+	if t.Hour() != r.Time.Hour() || t.Minute() != r.Time.Minute() {
+		return
+	}
+
+	standupers, err := b.db.ListChatStandupers(group.ChatID)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	if len(standupers) == 0 {
+		return
+	}
+
+	stillDidNotSubmit := map[string]int{}
+
+	for _, standuper := range standupers {
+		if b.submittedStandupToday(standuper) {
+			continue
+		}
+		stillDidNotSubmit["@"+standuper.Username] = standuper.Warnings
+	}
+
+	//? if everything is fine, should not bother the team...
+	if len(stillDidNotSubmit) == 0 {
+		return
+	}
+
+	var text string
+
+	for key, value := range stillDidNotSubmit {
+		text += fmt.Sprintf("Внимание, %v, до дедлайна осталось %v минут! Срочно пишите стендап и не подводите команду! Осталось пропусков: %v \n\n", key, warnPeriod, allowedSkips-value)
+	}
+
+	msg := tgbotapi.NewMessage(group.ChatID, text)
+	_, err = b.tgAPI.Send(msg)
+	if err != nil {
+		log.Error(err)
 	}
 }
 
@@ -84,28 +154,29 @@ func (b *Bot) NotifyGroup(group *model.Group, t time.Time) {
 	missed := map[string]int{}
 
 	for _, standuper := range standupers {
-		if !b.submittedStandupToday(standuper) {
-			if standuper.Warnings >= allowedSkips {
-				log.Infof("Missed more than %v standup! Should kick member!", allowedSkips)
-				resp, err := b.tgAPI.KickChatMember(tgbotapi.KickChatMemberConfig{
-					ChatMemberConfig: tgbotapi.ChatMemberConfig{
-						ChatID:             standuper.ChatID,
-						SuperGroupUsername: group.Username,
-						ChannelUsername:    standuper.Username,
-						UserID:             standuper.UserID,
-					},
-					UntilDate: time.Now().Unix(),
-				})
-				if err != nil {
-					log.Error("Failed to kick user: ", err)
-				}
-				log.Info(resp)
-				continue
-			}
-			standuper.Warnings++
-			missed["@"+standuper.Username] = standuper.Warnings
-			b.db.UpdateStanduper(standuper)
+		if b.submittedStandupToday(standuper) {
+			continue
 		}
+		if standuper.Warnings >= allowedSkips {
+			log.Infof("Missed %v standups! Should kick member!", allowedSkips)
+			resp, err := b.tgAPI.KickChatMember(tgbotapi.KickChatMemberConfig{
+				ChatMemberConfig: tgbotapi.ChatMemberConfig{
+					ChatID:             standuper.ChatID,
+					SuperGroupUsername: group.Username,
+					ChannelUsername:    standuper.Username,
+					UserID:             standuper.UserID,
+				},
+				UntilDate: time.Now().Unix(),
+			})
+			if err != nil {
+				log.Error("Failed to kick user: ", err)
+			}
+			log.Info(resp)
+			continue
+		}
+		standuper.Warnings++
+		missed["@"+standuper.Username] = standuper.Warnings
+		b.db.UpdateStanduper(standuper)
 	}
 
 	//? if everything is fine, should not bother the team...
