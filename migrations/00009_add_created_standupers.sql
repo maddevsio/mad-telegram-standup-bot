e00bf244e61bba7a125241cd41c79b1d259fb291
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-07-15 16:22:36 +0600

    Fix defaulting value

--- migrations/00009_add_created_standupers.sql
+++ migrations/00009_add_created_standupers.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE `standupers` ADD `created` DATETIME NOT NULL;
+ALTER TABLE `standupers` ADD `created` DATETIME NOT NULL DEFAULT '2019-01-01 00:00:00';
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-ALTER TABLE `standupers` DROP `created` DATETIME NOT NULL;
+ALTER TABLE `standupers` DROP `created`;
