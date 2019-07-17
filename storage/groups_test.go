52bbd5533529210861d5a38b624443bebed7dc4e
sula7 <gc738@mail.ru>
2019-07-17 18:04:58 +0600

    Test groups
    Coverage 53.7%

---
+++ storage/groups_test.go
@@ -0,0 +1,61 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/maddevsio/mad-internship-bot/model"
+
+	"github.com/maddevsio/mad-internship-bot/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGroups(t *testing.T) {
+	conf, err := config.Get()
+	require.NoError(t, err)
+	mysql, err := NewMySQL(conf)
+	require.NoError(t, err)
+
+	g := &model.Group{
+		ChatID:           int64(15),
+		Title:            "Chat",
+		Username:         "SulaFPV",
+		Description:      "foo",
+		TZ:               "GMT +6",
+		StandupDeadline:  "15 Oct",
+		Language:         "en",
+		OnbordingMessage: "Hello, user",
+		SubmissionDays:   "Everyday",
+		Advises:          "No standup bypass",
+	}
+
+	group, err := mysql.CreateGroup(g)
+	require.NoError(t, err)
+	require.Equal(t, "SulaFPV", group.Username)
+	require.Equal(t, int64(15), group.ChatID)
+	require.Equal(t, "GMT +6", group.TZ)
+
+	g.TZ = "Asia/Bishkek"
+
+	_, err = mysql.UpdateGroup(group)
+	require.NoError(t, err)
+	require.Equal(t, "Asia/Bishkek", group.TZ)
+
+	group, err = mysql.SelectGroup(g.ID)
+	require.NoError(t, err)
+
+	group, err = mysql.FindGroup(g.ChatID)
+	require.Equal(t, int64(15), g.ChatID)
+
+	group2, err := mysql.CreateGroup(g)
+	require.NoError(t, err)
+
+	groups, err := mysql.ListGroups()
+	require.NoError(t, err)
+	require.Equal(t, 2, len(groups))
+
+	err = mysql.DeleteGroup(group.ID)
+	require.NoError(t, err)
+
+	err = mysql.DeleteGroup(group2.ID)
+	require.NoError(t, err)
+}
