ccae99a0ef83420290fa28b72de1639962287c7b
Bekbolot <bekbolot@gmail.com>
2019-07-18 14:07:49 +0600

    Make new chages in tests

--- storage/standups_test.go
+++ storage/standups_test.go
@@ -21,12 +21,12 @@ func TestStandup(t *testing.T) {
 		Modified:  time.Date(2019, 7, 10, 1, 2, 0, 0, time.Local),
 		Username:  "User1",
 		Text:      "SomeText",
-		ChatID:    int64(1),
+		ChatID:    int64(3),
 	}
 
 	standup, err := mysql.CreateStandup(s)
 	require.NoError(t, err)
-	require.Equal(t, int64(1), standup.ChatID)
+	require.Equal(t, int64(3), standup.ChatID)
 	require.Equal(t, "User1", standup.Username)
 	require.Equal(t, "SomeText", standup.Text)
 
@@ -36,18 +36,18 @@ func TestStandup(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "NewText", u.Text)
 
-	standup, err = mysql.SelectStandup(standup.ID)
+	ch, err := mysql.SelectStandup(standup.ID)
 	require.NoError(t, err)
-	require.Equal(t, standup.ID, standup.ID)
+	require.Equal(t, standup.ID, ch.ID)
 
 	standup, err = mysql.SelectStandupByMessageID(standup.MessageID, standup.ChatID)
 	require.NoError(t, err)
 	require.Equal(t, 12345, standup.MessageID)
-	require.Equal(t, int64(1), standup.ChatID)
+	require.Equal(t, int64(3), standup.ChatID)
 
-	_, err = mysql.LastStandupFor(standup.Username, standup.ChatID)
+	ch, err = mysql.LastStandupFor(standup.Username, standup.ChatID)
 	require.NoError(t, err)
-	require.Equal(t, standup.ID, standup.ID)
+	require.Equal(t, standup.ID, ch.ID)
 
 	standup2, err := mysql.CreateStandup(s)
 	require.NoError(t, err)
