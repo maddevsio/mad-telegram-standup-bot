5a9761a5f20760b1f761a85fd27ec6b93cf42c61
Bekbolot <bekbolot@gmail.com>
2019-08-20 12:32:58 +0600

    Add test for function EditDeadline()

--- bot/commands_test.go
+++ bot/commands_test.go
@@ -705,3 +705,130 @@ func TestChangeUserTimeZone(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "You do not standup yet", text)
 }
+
+func TestLeaveStandupers(t *testing.T) {
+	Test = true
+	conf, err := config.Get()
+	require.NoError(t, err)
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+	_, err = bundle.LoadMessageFile("../active.en.toml")
+	require.NoError(t, err)
+	_, err = bundle.LoadMessageFile("../active.ru.toml")
+	require.NoError(t, err)
+
+	db, err := storage.NewMySQL(conf)
+	require.NoError(t, err)
+
+	bot := Bot{c: conf, db: db, bundle: bundle}
+
+	g := &model.Group{
+		ChatID:   int64(17),
+		Language: "en",
+	}
+
+	group, err := db.CreateGroup(g)
+	require.NoError(t, err)
+
+	team := &model.Team{
+		Group:    group,
+		QuitChan: make(chan struct{}),
+	}
+	bot.teams = append(bot.teams, team)
+
+	update := tgbotapi.Update{
+		Message: &tgbotapi.Message{
+			From: &tgbotapi.User{
+				ID: 2,
+			},
+			Chat: &tgbotapi.Chat{
+				ID: group.ChatID,
+			},
+		},
+	}
+
+	text, err := bot.LeaveStandupers(update)
+	require.NoError(t, err)
+	assert.Equal(t, "You do not standup yet", text)
+
+	g = &model.Group{
+		ChatID:   int64(18),
+		Language: "ru",
+	}
+
+	group, err = db.CreateGroup(g)
+	require.NoError(t, err)
+
+	team = &model.Team{
+		Group:    group,
+		QuitChan: make(chan struct{}),
+	}
+	bot.teams = append(bot.teams, team)
+
+	update = tgbotapi.Update{
+		Message: &tgbotapi.Message{
+			From: &tgbotapi.User{
+				ID: 3,
+			},
+			Chat: &tgbotapi.Chat{
+				ID: group.ChatID,
+			},
+		},
+	}
+
+	text, err = bot.LeaveStandupers(update)
+	require.NoError(t, err)
+	assert.Equal(t, "Вы еще не стендапите", text)
+}
+
+func TestEditDeadlines(t *testing.T) {
+	Test = true
+	conf, err := config.Get()
+	require.NoError(t, err)
+	db, err := storage.NewMySQL(conf)
+	require.NoError(t, err)
+	bundle := i18n.NewBundle(language.English)
+	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+	_, err = bundle.LoadMessageFile("../active.en.toml")
+	require.NoError(t, err)
+	_, err = bundle.LoadMessageFile("../active.ru.toml")
+	require.NoError(t, err)
+
+	bot := Bot{c: conf, db: db, bundle: bundle}
+	g := &model.Group{
+		ChatID:   int64(11),
+		Language: "en",
+	}
+
+	group, err := db.CreateGroup(g)
+	require.NoError(t, err)
+
+	update := tgbotapi.Update{
+		Message: &tgbotapi.Message{
+			Chat: &tgbotapi.Chat{
+				ID: group.ChatID,
+			},
+		},
+	}
+
+	infoMessage := "Standup deadline removed"
+	text, err := bot.EditDeadline(update)
+	require.NoError(t, err)
+	require.Equal(t, infoMessage, text)
+
+	group, err = db.FindGroup(group.ChatID)
+	require.NoError(t, err)
+	g = &model.Group{
+		ID:       group.ID,
+		Language: "ru",
+	}
+
+	group, err = db.UpdateGroup(g)
+	require.NoError(t, err)
+
+	infoMessage = "Крайний срок сдачи стендапов отменён"
+	text, err = bot.EditDeadline(update)
+	require.NoError(t, err)
+	require.Equal(t, infoMessage, text)
+
+}
