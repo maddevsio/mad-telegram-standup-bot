a7d2d59cd6c807bb255f651ec09f74999f3339fd
Bekbolot <bekbolot@gmail.com>
2019-08-22 15:28:57 +0600

    Implement db methods

---
+++ storage/notifications_thread.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"github.com/maddevsio/mad-telegram-standup-bot/model"
+)
+
+// CreateNotification create notifications
+func (m *MySQL) CreateNotification(s *model.NotificationThread) (*model.NotificationThread, error) {
+	res, err := m.conn.Exec(
+		"INSERT INTO `notifications_thread` (group_id, user_id, notification_time) VALUES (?, ?, ?)",
+		s.GroupID, s.UserID, s.NotificationTime,
+	)
+	if err != nil {
+		return nil, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	s.ID = id
+	return s, nil
+}
+
+// DeleteNotification deletes notification entry from database
+func (m *MySQL) DeleteNotification(id int64) error {
+	_, err := m.conn.Exec("DELETE FROM `notification_thread` WHERE id=?", id)
+	return err
+}
+
+// ListNotifications returns array of notifications entries from database
+func (m *MySQL) ListNotifications() ([]*model.NotificationThread, error) {
+	items := []*model.NotificationThread{}
+	err := m.conn.Select(&items, "SELECT * FROM `notifications_thread`")
+	return items, err
+}
