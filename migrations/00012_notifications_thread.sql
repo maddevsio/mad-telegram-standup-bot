f1ac3065908dab9860087e8869fc87f17266c1ab
Bekbolot <bekbolot@gmail.com>
2019-08-22 14:25:19 +0600

    Add field 'already_reminded' in notifications_thread migration

--- migrations/00012_notifications_thread.sql
+++ migrations/00012_notifications_thread.sql
@@ -4,7 +4,8 @@ CREATE TABLE `notifications_thread` (
     `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `group_id` BIGINT NOT NULL,
     `user_id` BIGINT NOT NULL,
-    `notification_time` VARCHAR(255) NOT NULL, 
+    `notification_time` VARCHAR(255) NOT NULL,
+    `already_reminded` INTEGER NOT NULL
 );
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
