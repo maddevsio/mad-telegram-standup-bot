d5a286267e54f72d9d5673a9e8e691ec423c0db3
Bekbolot <bekbolot@gmail.com>
2019-08-22 13:08:53 +0600

    Create mitgration notifications_thread

---
+++ migrations/00012_notifications_thread.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE `notifications_thread` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `group_id` BIGINT NOT NULL,
+    `user_id` BIGINT NOT NULL,
+    `notification_time` VARCHAR(255) NOT NULL, 
+);
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE `groups`;
