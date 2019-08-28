7fe1f5bb92692247e81dc5f07d1c0143ef0047d2
Bekbolot <bekbolot@gmail.com>
2019-08-28 16:39:10 +0600

    Implement aditional db methods and write tests

--- storage/notifications_thread.go
+++ storage/notifications_thread.go
@@ -4,8 +4,8 @@ import (
 	"github.com/maddevsio/mad-telegram-standup-bot/model"
 )
 
-// CreateNotification create notifications
-func (m *MySQL) CreateNotification(s model.NotificationThread) (model.NotificationThread, error) {
+// CreateNotificationThread create notifications
+func (m *MySQL) CreateNotificationThread(s model.NotificationThread) (model.NotificationThread, error) {
 	res, err := m.conn.Exec(
 		"INSERT INTO `notifications_thread` (chat_id, user_id, notification_time, reminder_counter) VALUES (?, ?, ?, ?)",
 		s.ChatID, s.UserID, s.NotificationTime, s.ReminderCounter,
@@ -21,15 +21,30 @@ func (m *MySQL) CreateNotification(s model.NotificationThread) (model.Notificati
 	return s, nil
 }
 
-// DeleteNotification deletes notification entry from database
-func (m *MySQL) DeleteNotification(id int64) error {
+// DeleteNotificationThread deletes notification entry from database
+func (m *MySQL) DeleteNotificationThread(id int64) error {
 	_, err := m.conn.Exec("DELETE FROM `notifications_thread` WHERE id=?", id)
 	return err
 }
 
-// ListNotifications returns array of notifications entries from database
-func (m *MySQL) ListNotifications() ([]model.NotificationThread, error) {
+// ListNotificationsThread returns array of notifications entries from database
+func (m *MySQL) ListNotificationsThread() ([]model.NotificationThread, error) {
 	items := []model.NotificationThread{}
 	err := m.conn.Select(&items, "SELECT * FROM `notifications_thread`")
 	return items, err
+}
+
+// SelectNotificationThread select Thread
+func (m *MySQL) SelectNotificationThread(userID int, chatID int64) (*model.NotificationThread, error) {
+	notification := &model.NotificationThread{}
+	err := m.conn.Get(notification, "SELECT * FROM `notifications_thread` WHERE user_id=? AND chat_id=?", userID, chatID)
+	return notification, err
+}
+
+// UpdateNotificationThread update field reminder counter
+func (m *MySQL) UpdateNotificationThread(id int64) error {
+	//nt := &model.NotificationThread{}
+	_, err := m.conn.Exec("UPDATE `notifications_thread` SET reminder_counter=reminder_counter+1 WHERE id=?", id)
+
+	return err
 }
--- storage/notifications_thread_test.go
+++ storage/notifications_thread_test.go
@@ -23,29 +23,59 @@ func TestNotification(t *testing.T) {
 		ReminderCounter:  0,
 	}
 
-	time := n.NotificationTime
+	timeTest := n.NotificationTime
 
-	notification, err := mysql.CreateNotification(n)
+	notification, err := mysql.CreateNotificationThread(n)
 	require.NoError(t, err)
 	assert.Equal(t, int64(1), notification.ChatID)
 	assert.Equal(t, 1, notification.UserID)
-	assert.Equal(t, time, notification.NotificationTime)
+	assert.Equal(t, timeTest, notification.NotificationTime)
 	assert.Equal(t, 0, notification.ReminderCounter)
 
-	notification2, err := mysql.CreateNotification(n)
+	notification2, err := mysql.CreateNotificationThread(n)
 	require.NoError(t, err)
 
-	notifications, err := mysql.ListNotifications()
+	notifications, err := mysql.ListNotificationsThread()
 	require.NoError(t, err)
 	assert.Equal(t, 2, len(notifications))
 
-	err = mysql.DeleteNotification(notification2.ID)
+	err = mysql.DeleteNotificationThread(notification2.ID)
 	require.NoError(t, err)
 
-	err = mysql.DeleteNotification(notification.ID)
+	err = mysql.DeleteNotificationThread(notification.ID)
 	require.NoError(t, err)
 
-	notifications, err = mysql.ListNotifications()
+	notifications, err = mysql.ListNotificationsThread()
 	require.NoError(t, err)
 	assert.Equal(t, 0, len(notifications))
+
+	n = model.NotificationThread{
+		ChatID:           int64(1),
+		UserID:           1,
+		NotificationTime: time.Now(),
+		ReminderCounter:  0,
+	}
+
+	nt, err := mysql.CreateNotificationThread(n)
+	require.NoError(t, err)
+
+	nThread, err := mysql.SelectNotificationThread(1, int64(1))
+	require.NoError(t, err)
+	assert.Equal(t, nThread.ID, nt.ID)
+
+	err = mysql.UpdateNotificationThread(nThread.ID)
+	require.NoError(t, err)
+
+	nThread, err = mysql.SelectNotificationThread(1, int64(1))
+	assert.Equal(t, nThread.ReminderCounter, 1)
+
+	err = mysql.UpdateNotificationThread(nThread.ID)
+	require.NoError(t, err)
+
+	nThread, err = mysql.SelectNotificationThread(1, int64(1))
+	assert.Equal(t, nThread.ReminderCounter, 2)
+
+	err = mysql.DeleteNotificationThread(nThread.ID)
+	require.NoError(t, err)
+
 }
